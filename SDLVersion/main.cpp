9d53fec559d6adcbcd44f8b866f9487134bb3f73
TheFakeMontyOnTheRun <danielmonteiro@id.uff.br>
2017-10-08 13:12:59 +0100

    Add fullscreen to Emscripten version

--- SDLVersion/main.cpp
+++ SDLVersion/main.cpp
@@ -13,6 +13,19 @@
 
 #endif
 
+
+
+#ifdef __EMSCRIPTEN__
+void enterFullScreenMode() {
+    EmscriptenFullscreenStrategy s;
+    memset(&s, 0, sizeof(s));
+    s.scaleMode = EMSCRIPTEN_FULLSCREEN_SCALE_ASPECT;
+    s.canvasResolutionScaleMode = EMSCRIPTEN_FULLSCREEN_CANVAS_SCALE_NONE;
+    s.filteringMode = EMSCRIPTEN_FULLSCREEN_FILTERING_DEFAULT;
+    emscripten_enter_soft_fullscreen(0, &s);
+}
+#endif
+
 std::shared_ptr<odb::CRenderer> renderer;
 odb::CGame game;
 
@@ -27,7 +40,7 @@ int main() {
     renderer = std::make_shared<odb::CRenderer>( game.getKeyPressedCallback(), game.getKeyReleasedCallback() );
 
 #ifdef __EMSCRIPTEN__
-    //  emscripten_request_fullscreen(0, 1);
+    enterFullScreenMode();
   emscripten_set_main_loop( gameLoopTick, 30, 1 );
 #else
     while ( true ) {
